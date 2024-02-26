80c6c1311fffdb19834537fdd094c0b3ead21916
Raphael Augusto Santos <rWX1294791@china.huawei.com>
2024-02-26 13:09:32 -0300

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ response = requests.get('https://api.jikan.moe/v4/anime')
 print(response.status_code)
 print(response.json())
 
-title = response.json()['data'][0]['url']
+title = response.json()['data'][4]['characters_staff']
 print(f'{title}')
 
-print('What type of ')
+print('What type of entertainment do you want today?')
